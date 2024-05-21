9c0fb1baf74d107f4c86bc829c5b1a7a342933ae
Anderson Sanches <andycds@gmail.com>
2024-05-20 21:46:30 -0300

    Microsserviço de Classificação

--- barramento-de-eventos/index.js
+++ barramento-de-eventos/index.js
@@ -5,12 +5,15 @@ app.use(express.json());
 
 app.post('/eventos', (req, res) => {
     const evento = req.body;
+    console.log("Chegou evento: " + evento.tipo);
     // envia o evento para o microsserviço de lembretes
     axios.post('http://localhost:4000/eventos', evento);
     // envia o evento para o microsserviço de observações
     axios.post('http://localhost:5000/eventos', evento);
     // envia o evento para o microsserviço de consulta
     axios.post('http://localhost:6000/eventos', evento);
+    // envia o evento para o microsserviço de classificação
+    axios.post('http://localhost:7000/eventos', evento);
     res.send({ msg: 'ok' });
 });
 
