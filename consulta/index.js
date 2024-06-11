e540b74013eea11d9875538b2c8f14f1189af4df
Anderson Sanches <andycds@gmail.com>
2024-06-10 21:14:14 -0300

    Remoção de comentário

--- consulta/index.js
+++ consulta/index.js
@@ -15,20 +15,6 @@ const pool = mysql.createPool({
     queueLimit: 0,
 }).promise();
 
-/*
-CREATE TABLE IF NOT EXISTS lembrete (
-    id int primary key not null,
-    texto varchar(200) not null
-);
-
-CREATE TABLE IF NOT EXISTS observacao (
-    id varchar(36) primary key not null,
-    lembrete_id int not null,
-    texto varchar(200) not null,
-    status varchar(30) not null
-);
-*/
-
 const funcoes = {
     LembreteCriado: (lembrete) => {
         pool.getConnection()
